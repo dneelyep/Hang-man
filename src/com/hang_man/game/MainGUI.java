a6677c84b989b6357df34fbdbec197261cb359a8
Daniel Neel <dneelyep@gmail.com>
2012-08-14 09:02:25 -0400

    Started the constructor for MainGUI, made some notes for the game.
    Now, MainGUI actually creates a GUI.
    Also, in the notes file are outlined some implementation ideas and a grid
    which lays out how I want to set up the board.

--- src/com/hang_man/game/MainGUI.java
+++ src/com/hang_man/game/MainGUI.java
@@ -1,10 +1,11 @@
 package com.hang_man.game;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 /** MainGUI.java - Main class for the hang man game. This class
- *                  handles setting up the GUI and coordinating game
- *                  state changes.
+ *                 handles setting up the GUI and coordinating game
+ *                 state changes.
  *
  * @author Daniel Neel */
 public class MainGUI extends JFrame { 
@@ -13,6 +14,27 @@ public class MainGUI extends JFrame {
      *  to set up GUI components. */
     public static void main(String args[]) {
 	new MainGUI();
-	System.out.println("Testing this.");
     }
+
+    /** When a MainGUI object is created, set relevant properties for
+     *  MainGUI's frame, call the method responsible for creating components,
+     *  and make the frame visible. */
+    public MainGUI() {
+        setSize(400, 300);
+	setTitle("Hang man");
+	setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+	initControls();
+
+	pack();
+	setVisible(true);
+    }
+
+    /** Create all controls that go in the interface, and place
+     *  them inside this MainGUI. */
+    private void initControls() {
+	add(new JLabel("Testing"));
+    }
+
+11x7
 }
